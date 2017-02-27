2a777b92f8b15c11f6d469dbaea48c8a1453e71f
Kalvin Manson <kalvinmanson@gmail.com>
2017-02-27 17:40:28 -0500

    analytics

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -65,7 +65,7 @@ Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+//Route::get('auth/register', 'Auth\AuthController@getRegister');
+//Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 
